52e247bc0e0ded010df080af2027638aee8eb95a
Chris Hawkins <94678272+csnhawkins@users.noreply.github.com>
2024-09-26 09:26:42 +0100

    Update dbo.WidgetPrices.sql - Change default Data Type to Money

--- schema-model/Tables/dbo.WidgetPrices.sql
+++ schema-model/Tables/dbo.WidgetPrices.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[WidgetPrices]
 (
 [RecordID] [int] NOT NULL IDENTITY(1, 1),
 [WidgetID] [int] NULL,
-[Price] [decimal] (18, 0) NULL
+[Price] [money] NULL
 )
 GO
 ALTER TABLE [dbo].[WidgetPrices] ADD CONSTRAINT [PK_WidgetPrices] PRIMARY KEY NONCLUSTERED ([RecordID])
