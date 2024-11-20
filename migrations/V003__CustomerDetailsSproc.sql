44affc85ac5da85072a3f3eda9e9b46a7e9d01cb
chris.hawkins@red-gate.com <chris.hawkins@red-gate.com>
2024-11-20 15:25:09 +0000

    Created deployment for created new sproc

---
+++ migrations/V003__CustomerDetailsSproc.sql
@@ -0,0 +1,24 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [Customers].[CustomerDetails]'
+GO
+--SET QUOTED_IDENTIFIER ON|OFF
+--SET ANSI_NULLS ON|OFF
+--GO
+CREATE PROCEDURE [Customers].[CustomerDetails]
+--    @parameter_name AS INT
+-- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
+AS
+BEGIN
+    SELECT CustomerID,
+           FirstName,
+           LastName,
+           Email,
+           DateOfBirth,
+           Phone,
+           Address FROM Customers.Customer
+END
+GO
+
