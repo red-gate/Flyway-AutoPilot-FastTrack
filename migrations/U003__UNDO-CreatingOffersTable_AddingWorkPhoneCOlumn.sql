e4f2ef31ead0ef9841f51804de5a38817bea8724
Customer Training <CustomerVM@red-gate.com>
2025-02-26 16:42:25 +0000

    Created new offers table and WorkPhone column deployment script

---
+++ migrations/U003__UNDO-CreatingOffersTable_AddingWorkPhoneCOlumn.sql
@@ -0,0 +1,22 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Dropping [Customers].[Offers]'
+GO
+DROP TABLE [Customers].[Offers]
+GO
+PRINT N'Altering [Customers].[Customer]'
+GO
+ALTER TABLE [Customers].[Customer] DROP
+COLUMN [WorkPhone]
+GO
+PRINT N'Refreshing [Sales].[CustomerOrdersView]'
+GO
+EXEC sp_refreshview N'[Sales].[CustomerOrdersView]'
+GO
+PRINT N'Refreshing [Customers].[CustomerFeedbackSummary]'
+GO
+EXEC sp_refreshview N'[Customers].[CustomerFeedbackSummary]'
+GO
+
---
+++ migrations/V003__CreatingOffersTable_AddingWorkPhoneCOlumn.sql
@@ -0,0 +1,26 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [Customers].[Customer]'
+GO
+ALTER TABLE [Customers].[Customer] ADD
+[WorkPhone] [int] NULL
+GO
+PRINT N'Refreshing [Customers].[CustomerFeedbackSummary]'
+GO
+EXEC sp_refreshview N'[Customers].[CustomerFeedbackSummary]'
+GO
+PRINT N'Refreshing [Sales].[CustomerOrdersView]'
+GO
+EXEC sp_refreshview N'[Sales].[CustomerOrdersView]'
+GO
+PRINT N'Creating [Customers].[Offers]'
+GO
+CREATE TABLE [Customers].[Offers]
+(
+[id] [int] NOT NULL,
+[name] [varchar] (50) NOT NULL
+)
+GO
+
